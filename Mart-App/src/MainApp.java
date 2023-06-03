ce0aa2edac8af3b8456fc00c87f73ad463dab405
faqih <mohfaqihrahakbauw@gmail.com>
2023-06-03 13:13:20 +0700

    Revisi

--- Mart-App/src/MainApp.java
+++ Mart-App/src/MainApp.java
@@ -9,18 +9,18 @@ public class MainApp {
         employeeMart.contactNum = "081100938847";
         employeeMart.entityNum = "PT.Syntechsia Sejahtera";
         System.out.println("Mart Identity");
-        System.out.println("Locatin         : " + employeeMart.location);
-        System.out.println("Identity Number : " + employeeMart.identityNum);
-        System.out.println("Contact Number  : " + employeeMart.contactNum);
-        System.out.println("Entity Name     : " + employeeMart.entityNum);
+        System.out.println("Locatin         : " .concat(employeeMart.location));
+        System.out.println("Identity Number : " .concat(employeeMart.identityNum));
+        System.out.println("Contact Number  : " .concat(employeeMart.contactNum));
+        System.out.println("Entity Name     : " .concat(employeeMart.entityNum));
         System.out.println("==========================================");
         employeeMart.name = "Petruk Silailai";
         employeeMart.position = "Manager";
         employeeMart.sallary = "50000000";
         System.out.println("Employee of PT.Syntechsia Sejahtera");
-        System.out.println("Name     : " + employeeMart.name);
-        System.out.println("Position : " + employeeMart.position);
-        System.out.println("Sallary  : " + employeeMart.sallary);
+        System.out.println("Name     : " .concat(employeeMart.name));
+        System.out.println("Position : " .concat(employeeMart.position));
+        System.out.println("Sallary  : " .concat(employeeMart.sallary));
         employeeMart.employee();
     }
 }
--- Mart-App/src/mart/SyntechsiaMart.java
+++ Mart-App/src/mart/SyntechsiaMart.java
@@ -1,7 +1,6 @@
 package mart;
 
 public abstract class SyntechsiaMart {
-
     public String location;
     public String identityNum;
     public String contactNum;
